ab53c29ac89471821d5fb7ea9d2f9247c7c18700
jmu22 <jmu22@bu.edu>
2021-01-20 14:16:44 -0500

    final

--- aws_api.py
+++ aws_api.py
@@ -1,1 +1,30 @@
-print("hello world2")
+import requests
+import os
+
+from requests.auth import HTTPBasicAuth
+
+
+inputfile = open('aws_accounts.txt')
+headers={'Content-type':'application/json', 'Accept':'application/json'}
+
+for line in inputfile:
+    line = line.strip('\n')
+    myobj = {
+        "account_id": line,
+        "access_role_name": "svc-operations-snow-crossaccount",
+        "accessor_id": "142855751540"
+    }
+    response = requests.post("https://asuriondev.service-now.com/api/asrp/aws_endpoint/snowdiscovery", json = myobj, headers = headers,  auth=HTTPBasicAuth(os.environ.get('asurion_user'),os.environ.get('asurion_pass')))
+    if (response.status_code != 200):
+        print("Failed to discover datacenters for account " + line + " The error is " + str(response.status_code))
+    else:
+        print("Successfully added AWS Account " + line)
+print("Job has finished")
+
+
+
+
+
+
+
+
